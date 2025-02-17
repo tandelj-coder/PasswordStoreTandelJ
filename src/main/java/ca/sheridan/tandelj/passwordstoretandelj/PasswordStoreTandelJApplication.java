1c918d1cca0af0d3ec39bc94c2a7f8e748a31923
Jigar Tandel <tandel@sheridancollege.ca>
2025-02-17 14:44:51 -0500

    Initial Project

---
+++ src/main/java/ca/sheridan/tandelj/passwordstoretandelj/PasswordStoreTandelJApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridan.tandelj.passwordstoretandelj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordStoreTandelJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordStoreTandelJApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/sheridan/tandelj/passwordstoretandelj/PasswordStoreTandelJApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridan.tandelj.passwordstoretandelj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordStoreTandelJApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
