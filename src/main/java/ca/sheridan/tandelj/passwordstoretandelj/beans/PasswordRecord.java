45dc59e0a95df10474a5763deb317e3397679a4e
Jigar Tandel <tandel@sheridancollege.ca>
2025-02-17 16:47:16 -0500

    PasswordRecord.java is modified

--- src/main/java/ca/sheridan/tandelj/passwordstoretandelj/beans/PasswordRecord.java
+++ src/main/java/ca/sheridan/tandelj/passwordstoretandelj/beans/PasswordRecord.java
@@ -1,16 +1,16 @@
 package ca.sheridan.tandelj.passwordstoretandelj.beans;
 
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 public class PasswordRecord {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String username;
